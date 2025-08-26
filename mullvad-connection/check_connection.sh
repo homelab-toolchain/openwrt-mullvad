93fef2520e84d4968b535a0303a1f5fe0d06fd97
ahmad2702 <ahmad2702@users.noreply.github.com>
2025-08-26 09:24:20 +0500

    feat: add improvements

--- mullvad-connection/check_connection.sh
+++ mullvad-connection/check_connection.sh
@@ -11,12 +11,17 @@ IP=$(echo "$MULLVAD_INFO" | jq -r '.ip')
 COUNTRY=$(echo "$MULLVAD_INFO" | jq -r '.country')
 CITY=$(echo "$MULLVAD_INFO" | jq -r '.city')
 
-CURRENT_LOCATION="$CITY, $COUNTRY"
-EXPECTED_LOCATION="Amsterdam, Netherlands"
+#CURRENT_LOCATION="$CITY, $COUNTRY"
+#EXPECTED_LOCATION="Amsterdam, Netherlands"
 
 if [ "$IS_MULLVAD_NETWORK" != "true" ]; then
     echo "Something is wrong. Rebooting..."
     reboot
 else
     echo -e "There is Mullvad network!\n\nLocation: $CITY, $COUNTRY\nIP: $IP"
+    
+    echo -e "\n" >> /scripts/hc-ping.log
+    echo "$(date '+%Y-%m-%d %H:%M:%S') - Running health check" >> /scripts/hc-ping.log
+    curl -fsS -m 10 --retry 5 https://hc-ping.com/e3bdc3b5-77e6-4b30-97a9-ea7037a0b553 >> /scripts/hc-ping.log 2>&1
+    echo -e "\n" >> /scripts/hc-ping.log
 fi
